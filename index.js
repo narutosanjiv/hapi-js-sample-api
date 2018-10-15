dc2692cb8d6c9c3ef20e6e829d064ad0a02669b1
Sanjiv Kumar  Jha <doc@Sanjivs-MacBook-Pro.local>
2018-10-16 01:48:33 +0530

    Added host 0.0.0.0

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const server_opts = {
 const init = async() =>{
     const server = Hapi.server({
         port: process.env.PORT || 3000,
-        host: 'localhost',
+        host: '0.0.0.0',
         routes: {cors: {origin: ['*']} }
     });
 
