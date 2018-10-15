0c4bc1c3cf4225939357ba7895ab5f57471b1654
Sanjiv Kumar  Jha <doc@Sanjivs-MacBook-Pro.local>
2018-10-16 01:39:43 +0530

    Added PORT for nodejs

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const server_opts = {
 }
 const init = async() =>{
     const server = Hapi.server({
-        port: 3000,
+        port: process.env.PORT || 3000,
         host: 'localhost',
         routes: {cors: {origin: ['*']} }
     });
